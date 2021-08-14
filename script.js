9aeb5b541451089905734d6b9eee5543c2d75519
unknown <maluribeiro08@gmail.com>
2021-08-14 17:04:59 -0300

    Terminando projeto da calculadora IMC

--- script.js
+++ script.js
@@ -1,13 +1,63 @@
-const nome = document.getElementById("nome")
-const altura = document.getElementById("altura")
-const peso = document.getElementById("peso")
-const calcular = document.getElementById("calcular")
-const resultado = document.getElementById("resultado")
+
+const calcular = document.getElementById('calcular');
+
 
 
 function calcularImc ()
 {
-    resultado.value = parseInt((peso * (altura*altura)))
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== ''  && peso !== '')
+    {
+        const imc = (peso / (altura * altura)).toFixed(2);
+        //toFixed arredonda os números de acordo com o n° de casas decimais do ()
+
+
+        let classificao = "";
+
+        if (imc < 18.5)
+        {
+            classificao = 'abaixo do peso :|';
+        }
+
+        else if ( imc >= 18.5 && imc < 25)
+        {
+            classificao = "com peso ideal. Parabéns!! :)";
+        }
+
+        else if (imc >= 25 && imc <30)
+        {
+            classificao = "levemente acima do peso :|";
+        }
+
+        else if (imc >=30 && imc <35)
+        {
+            classificao = "com obesidade grau I :|";
+        }
+
+        else if (imc >=35 && imc <40)
+        {
+            classificao = "com obesidade grau II :|";
+        }
+
+        else
+        {
+            classificao = "com obesidade grau III :| Cuidado!";
+        }
+
+
+        resultado.textContent = `${nome}, seu IMC é ${imc} e você está ${classificao}`;
+    }
+
+    else
+    {
+        resultado.textContent = "Preencha todos os campos";
+    }
+
+
 }
 
-calcular.addEventListener("click", calcularImc)
+calcular.addEventListener('click', calcularImc);
